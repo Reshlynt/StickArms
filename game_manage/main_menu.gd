c32dd2f09d9dc87da9b5943dd263170270512492
unknown <sj49@email.sc.edu>
2024-10-24 16:01:51 -0400

    Settings Button Connection

--- game_manage/main_menu.gd
+++ game_manage/main_menu.gd
@@ -6,7 +6,16 @@ func _ready() -> void:
 #change scene to game
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://game_manage/StickArmsGame.tscn")
+	
+
 
 # quits the game entirely
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_settings_pressed() -> void:
+	#print("LOAD OPTIONS MENU")
+	get_tree().change_scene_to_file("res://game_manage/Settings.tscn")
+	#change to SettingsPage Later
